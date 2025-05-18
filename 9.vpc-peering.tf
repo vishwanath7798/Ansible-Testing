160a891a3afe51d4ab6a4d9730bb4e104174765e
vishwanath7798 <vishwanathspatil07@gmail.com>
2025-05-18 23:54:45 +0530

    added all ansible master files

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -1,9 +1,9 @@
 data "aws_vpc" "ansible_vpc" {
-  id = "vpc-0f362095fed5e9d9c"
+  id = "vpc-04f50ef1a530f547e"
 }
 
 data "aws_route_table" "ansible_vpc_rt" {
-  subnet_id = "subnet-0a8684236815bef1c"
+  subnet_id = "subnet-086527252e198eb38"
   #If subnet_id giving errors use route table id as below
   #route_table_id = data.aws_route_table.ansible_vpc_rt.id
 }
